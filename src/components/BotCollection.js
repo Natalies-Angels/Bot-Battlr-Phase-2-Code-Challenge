22d4455d549a1bc2cef7acfe48762e9dba36d4f2
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-09-05 11:21:50 +0300

    Rendered BotCollection component

---
+++ src/components/BotCollection.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from "react";
+import BotCard from "./BotCard";
+function BotCollection() {
+  const [bots, setBots] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:8002/bots')
+    .then(response => response.json())
+    .then(bots => setBots(bots))
+  }, []);
+
+  return (
+    <div className="ui four column grid">
+      <div className="row">
+        
+        {bots.map(bot => ( 
+        <div key={bot.id}>
+        <BotCard bot={bot} />
+      </div>))}
+      </div>
+     
+    </div>
+  );
+}
+
+export default BotCollection;
