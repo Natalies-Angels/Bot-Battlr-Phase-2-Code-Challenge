b28a4b2c298c532d91c8e5b4201c558b98e3254f
Natalie <137040990+Natalies-Angels@users.noreply.github.com>
2023-09-06 14:10:57 +0300

    Update BotsPage.js

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -5,44 +5,43 @@ import BotCollection from "./BotCollection";
 function BotsPage() {
   const [botsData, setBotsData] = useState([]);
   const [clickedBots, setClickedBots] = useState([]);
-  const [botsInArmy, setBotsInArmy] = useState([])
-  
-   useEffect(()=>{
-    fetch('http://localhost:8002/bots')
-    .then(response => response.json())
-    .then(botsData => setBotsData(botsData))
-    .catch((error) => console.error("Failed to fech Bot Data", error))
-   },[]);
-  
-   function sendBotToArmy(bot)
-   {
-     if(!clickedBots.some((clickedBot) => (clickedBot.id) === bot.id)) //uses some() to check if the bot exists in the array
-     {
-        setClickedBots([...clickedBots, bot])//if bot does not exist in the array, it's added tothe clickedBots array
-    
-        if(!botsInArmy.includes(bot.id))
-         {
-          setBotsInArmy([...botsInArmy, bot.id])
-         }
-     } 
-     else
-     {
-        window.alert(`${bot.name} is already enlisted in the army!`)
-     }
-   }
+  const [botsInArmy, setBotsInArmy] = useState([]);
 
-   function removeBotFromArmy(botId)
-   {
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((response) => response.json())
+      .then((botsData) => setBotsData(botsData))
+      .catch((error) => console.error("Failed to fetch Bot Data", error));
+  }, []);
+
+  function handleDeleteBot(deletedBot) {
+    const dischargedBots = botsData.filter((bot) => bot.id !== deletedBot.id);
+    setBotsData(dischargedBots);
+  }
+
+  function sendBotToArmy(bot) {
+    if (!clickedBots.some((clickedBot) => clickedBot.id === bot.id)) {
+      setClickedBots([...clickedBots, bot]);
+
+      if (!botsInArmy.includes(bot.id)) {
+        setBotsInArmy([...botsInArmy, bot.id]);
+      }
+    } else {
+      window.alert(`${bot.name} is already enlisted in the army!`);
+    }
+  }
+
+  function removeBotFromArmy(botId) {
     const updatedBotArmy = clickedBots.filter((bot) => botId !== bot.id);
     setClickedBots(updatedBotArmy);
-   }
+  }
 
   return (
     <div>
-      <YourBotArmy clickedBots={clickedBots} removeBotFromArmy={removeBotFromArmy}/>
-      <BotCollection botsData={botsData} sendBotToArmy={sendBotToArmy} />
+      <YourBotArmy clickedBots={clickedBots} removeBotFromArmy={removeBotFromArmy} />
+      <BotCollection botsData={botsData} sendBotToArmy={sendBotToArmy} handleDeleteBot={handleDeleteBot} />
     </div>
-  )
+  );
 }
 
 export default BotsPage;
